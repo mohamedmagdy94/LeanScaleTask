1a47fd3a4e0c84c84c6042848d0429e29c6314f7
Mohammed Eltaweel <31571400+mohamedmagdy94@users.noreply.github.com>
2021-09-04 22:28:04 +0200

    Add load image async extension

---
+++ LeanScaleTask/SupportingFiles/Extensions/UIImageView+Extension.swift
@@ -0,0 +1,39 @@
+//
+//  UIImageView+Extension.swift
+//  LeanScaleTask
+//
+//  Created by Mohamed Eltaweel on 04/09/2021.
+//
+
+import UIKit
+
+let imageCache = NSCache<NSString, AnyObject>()
+
+extension UIImageView {
+    func loadImageUsingCache(withUrl urlString : String) {
+        let url = URL(string: urlString)
+        self.image = nil
+
+        // check cached image
+        if let cachedImage = imageCache.object(forKey: urlString as NSString) as? UIImage {
+            self.image = cachedImage
+            return
+        }
+
+        // if not, download image from url
+        URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
+            if error != nil {
+                print(error!)
+                return
+            }
+
+            DispatchQueue.main.async {
+                if let image = UIImage(data: data!) {
+                    imageCache.setObject(image, forKey: urlString as NSString)
+                    self.image = image
+                }
+            }
+
+        }).resume()
+    }
+}
