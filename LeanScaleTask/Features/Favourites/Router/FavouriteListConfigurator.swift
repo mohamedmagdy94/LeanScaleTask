17e626a29516777608c52a5c2e755130c75aee06
Mohammed Eltaweel <31571400+mohamedmagdy94@users.noreply.github.com>
2021-09-06 00:48:40 +0200

    Finish Router

---
+++ LeanScaleTask/Features/Favourites/Router/FavouriteListConfigurator.swift
@@ -0,0 +1,8 @@
+//
+//  FavouriteListConfigurator.swift
+//  LeanScaleTask
+//
+//  Created by Mohamed Eltaweel on 06/09/2021.
+//
+
+import Foundation
---
+++ LeanScaleTask/Features/Favourites/Router/FavouriteListNavigationRouter.swift
@@ -0,0 +1,8 @@
+//
+//  FavouriteListNavigationRouter.swift
+//  LeanScaleTask
+//
+//  Created by Mohamed Eltaweel on 06/09/2021.
+//
+
+import Foundation
